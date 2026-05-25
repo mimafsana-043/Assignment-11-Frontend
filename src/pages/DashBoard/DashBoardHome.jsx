c37f6b3cb3e7d20d6d3851fe1bedbc524085f36a
Mimnoob <hamza197124@gmail.com>
2026-05-26 00:09:11 +0600

    Dashboard

--- src/pages/DashBoard/DashBoardHome.jsx
+++ src/pages/DashBoard/DashBoardHome.jsx
@@ -5,7 +5,7 @@ const DashboardHome = () => {
 
   return (
     <div className="bg-white p-8 rounded-2xl shadow">
-      <h1 className="text-3xl font-bold mb-2">Welcome to Dashboard</h1>
+      <h1 className="text-3xl font-bold mb-2 text-indigo-600">Welcome to Dashboard</h1>
       <p className="text-gray-500">Your current role is: <span className="font-bold text-indigo-600">{role}</span></p>
     </div>
   );
