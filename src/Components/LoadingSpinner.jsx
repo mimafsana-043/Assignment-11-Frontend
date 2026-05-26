ebc74f190f547362c76fe9eac30fcddee17f37fa
Mimnoob <hamza197124@gmail.com>
2026-05-26 21:43:04 +0600

    removed spinner error

--- src/Components/LoadingSpinner.jsx
+++
@@ -1,9 +0,0 @@
-const LoadingSpinner = () => {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50">
-      <div className="w-14 h-14 border-4 border-indigo-200 border-t-indigo-600 rounded-full animate-spin"></div>
-    </div>
-  );
-};
-
-export default LoadingSpinner;
